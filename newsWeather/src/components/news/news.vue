c2f2dcc7038a103a342c55ad382c7e0064db9140
Neo <969504161@qq.com>
2018-04-08 18:11:24 +0800

    新闻列表信息

--- newsWeather/src/components/news/news.vue
+++ newsWeather/src/components/news/news.vue
@@ -4,14 +4,23 @@
 
       这里是新闻显示页面
 
+      <template v-for="item in newsList">
+        <div class="news-item">
+            省份：{{item.shidu}}
+            省份：{{item.shidu}}
+        </div>
+      </template>
+
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
   data () {
     return {
+      newsList : ''   // 新闻列表信息
 
     };
   },
@@ -20,7 +29,23 @@ export default {
 
   computed: {},
 
-  methods: {}
+  methods: {
+
+  },
+
+  mounted(){
+
+      axios.get(Api_Proxy + "http://www.sojson.com/open/api/weather/json.shtml?city=广州").then( res => {
+        this.newsList = res.data;
+        console.log(res.data);
+      }).catch( err => {
+        console.log(err);
+      });
+
+
+
+  }
+
 }
 
 </script>
@@ -33,6 +58,13 @@ export default {
   top: 55px;
   bottom: 55px;
   overflow-y: auto;
+}
+
+.news-item{
+  border-bottom: 1px solid rgb(239, 240, 241);
+  width: 100%;
+  height: 50px;
+  background-color: rgb(239, 240, 241);
 }
 
 
