bfef986338d710fbfbf43174a54d8379dc5c1214
Neo <969504161@qq.com>
2018-04-08 18:10:57 +0800

    添加mock数据

--- newsWeather/src/mock/mock.js
+++ newsWeather/src/mock/mock.js
@@ -14,3 +14,53 @@ Mock.mock("http://neokekeke/getPerson",
 Mock.mock("https://api.douban.com/v2/" , "GET" , function(data){
     return data;
 });
+
+Mock.mock("http://neokekeke/getWeather" , [
+  {
+      "province" : "广东省",
+      "city" : [
+          {
+            "GZ" : {
+              "area1" : "花都区 晴",
+              "area2" : "白云区 多云"
+            }
+          },
+          {
+            "SZ" : {
+              "area1" : "罗湖区 晴",
+              "area2" : "福田区 蒙蒙雨"
+            }
+          },
+          {
+            "ZH" : {
+              "area1" : "香洲区 晴",
+              "area2" : "斗门区 微风"
+            }
+          }
+      ]
+  },
+  {
+    "province" : "广东省",
+    "city" : [
+        {
+          "GZ" : {
+            "area1" : "花都区 晴",
+            "area2" : "白云区 多云"
+          }
+        },
+        {
+          "SZ" : {
+            "area1" : "罗湖区 晴",
+            "area2" : "福田区 蒙蒙雨"
+          }
+        },
+        {
+          "ZH" : {
+            "area1" : "香洲区 晴",
+            "area2" : "斗门区 微风"
+          }
+        }
+    ]
+}
+
+]);
