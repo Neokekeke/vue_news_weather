e3d221167fb6432b0a119c415e59546331f92ff2
Neo <969504161@qq.com>
2018-04-11 11:44:59 +0800

    fix：y轴滚动条的问题 ； fix：fixed定位和绝对定位相互遮盖的问题，z-index解决

---
+++ vue_news_weather/src/components/common/navBack.vue
@@ -0,0 +1,24 @@
+<!-- 顶部通用返回键 -->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {}
+}
+
+</script>
+<style scoped>
+</style>
--- vue_news_weather/src/components/movie/movieDetail.vue
+++ vue_news_weather/src/components/movie/movieDetail.vue
@@ -1,17 +1,35 @@
 <!-- 电影详情页 -->
 <template>
-  <div>
+
+   <div class="outter">
+
+    <div class="navBack">
+         返回
+         <span>{{movieData.title}}</span>
+    </div>
+
+    <!-- 电影详情介绍 -->
+    <div class="container">
+      <div class="box">
+        <div class="moviePic">
+            <img :src="movieData.images.large"/>
+        </div>
+        <div class="movieDesc">
+            {{movieData.summary}}
 
 
-    {{movieData.summary}}
-
-
+        </div>  <p>
+              {{movieData.directors[0].name}}
+            </p>
+      </div>
+    </div>
 
   </div>
 </template>
 
 <script>
 import ajax from '../common/ajax.js';
+import navBack from '../common/navBack.vue';
 
 export default {
   data () {
@@ -21,12 +39,12 @@ export default {
     };
   },
 
-  components: {},
-
-  computed: {},
+  components: {
+      navBack
+  },
 
   mounted() {
-    console.log(this.$route.params.id);
+
     ajax.jqGet(this.detailUrl).then(res => {
       this.movieData = res;
       console.log(res);
@@ -39,4 +57,65 @@ export default {
 
 </script>
 <style scoped>
+.outter{
+  position: relative;
+}
+
+.navBack{
+  position: fixed;
+  top: 0;
+  height: 50px;
+  max-width: 1024px;
+  width: 100%;
+  background-color: lightcoral;
+  z-index: 100;   /* 这里是关键避免absolute，覆盖fixed */
+}
+
+.navBack span{
+  text-align: center;
+  line-height: 50px;
+}
+
+/* 隐藏滚动条的精髓是div高度要定，宽度可以百分比，针对y轴，x轴亦然 */
+.container{
+  max-width: 1024px;
+  width: 100%;
+  height: 1000px;
+  position: relative;
+  overflow: hidden;
+  margin: 0 auto;
+  top: 50px;
+  z-index: 99;
+}
+
+.box{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 1000px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+
+/* 电影海报显示 ，电影描述显示 */
+.moviePic, .movieDesc{
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.moviePic img{
+  width: 100%;
+  height: 100%;
+}
+
+ /* for Chrome */
+.box::-webkit-scrollbar {
+    display: none;
+}
+
+
 </style>
